a72c28c2ebfee90db349f181bf19858beacfb015
duyenduyen8301 <165978123+duyenduyen8301@users.noreply.github.com>
2025-05-07 09:08:39 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import SchedulePage from "./schedule/page";
-import TripDetailsModal from "@/components/modal/TripDetail";
+
 
 export default function Home() {
   return (
     <div>
-      {/* <SchedulePage /> */}
-      <TripDetailsModal />
+       <SchedulePage /> 
+     
     </div>
   );
 }
